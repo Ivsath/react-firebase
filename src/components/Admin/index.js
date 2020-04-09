be6b25d76c1434897ea7ba8f95337613071b6718
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-09 12:26:23 -0500

    Fetch users from the Firebase realtime database API

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Admin = () => (
-  <div>
-    <h1>Admin</h1>
-  </div>
-);
+import { withFirebase } from "../Firebase";
 
-export default Admin;
+class AdminPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      users: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    // The on() method registers a continuous listener
+    // that triggers every time something has changed, the once() method registers a listener that would
+    // be called only once. In this scenario, we are interested to keep the latest list of users.
+    this.props.firebase.users().on("value", (snapshot) => {
+      this.setState({
+        users: snapshot.val(),
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Admin</h1>
+      </div>
+    );
+  }
+}
+
+export default withFirebase(AdminPage);
