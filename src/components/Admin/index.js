1a2bb60f2201b526706e8396882e879aa0ecefdc
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-09 12:30:31 -0500

    Restructure the users retrieved from the firebase database as list and remove the listener

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -8,7 +8,7 @@ class AdminPage extends Component {
 
     this.state = {
       loading: false,
-      users: {},
+      users: [],
     };
   }
 
@@ -19,11 +19,21 @@ class AdminPage extends Component {
     // that triggers every time something has changed, the once() method registers a listener that would
     // be called only once. In this scenario, we are interested to keep the latest list of users.
     this.props.firebase.users().on("value", (snapshot) => {
+      const usersObject = snapshot.val();
+      const usersList = Object.keys(usersObject).map((key) => ({
+        ...usersObject[key],
+        uid: key,
+      }));
+
       this.setState({
-        users: snapshot.val(),
+        users: usersList,
         loading: false,
       });
     });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
   }
 
   render() {
