f4182570cb3191102821f7234135ec0136762deb
Irvin Reyes <reyes.yerp@gmail.com>
2020-04-10 10:08:38 -0500

    Add google social login using the provider

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -11,6 +11,7 @@ const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <SignInGoogle />
     <PasswordForgetLink />
     <SignUpLink />
   </div>
@@ -79,8 +80,54 @@ class SignInFormBase extends Component {
   }
 }
 
+class SignInGoogleBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { error: null };
+  }
+
+  onSubmit = (event) => {
+    this.props.firebase
+      .doSignInWithGoogle()
+      .then((socialAuthUser) => {
+        // Create a user in the Firebase Realtime Database too.
+        // To only create the user once with a social login, make use of the
+        // socialuser.additionalUserInfo.isNewUser property to only create a new user when signing in
+        // with Google for the first time.
+        return this.props.firebase.user(socialAuthUser.user.uid).set({
+          username: socialAuthUser.user.displayName,
+          email: socialAuthUser.user.email,
+          roles: {},
+        });
+      })
+      .then(() => {
+        this.setState({ error: null });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  render() {
+    const { error } = this.state;
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <button type="submit">Sign In with Google</button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
 const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
+
+const SignInGoogle = compose(withRouter, withFirebase)(SignInGoogleBase);
 
 export default SignInPage;
 
-export { SignInForm };
+export { SignInForm, SignInGoogle };
